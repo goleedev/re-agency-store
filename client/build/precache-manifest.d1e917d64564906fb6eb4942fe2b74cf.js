b896ba9096d89cb088df1ee2d276e395baf10416
GO Lee <golee.dev@gmail.com>
2020-07-10 18:00:15 +0900

    heroku deploy

---
+++ client/build/precache-manifest.d1e917d64564906fb6eb4942fe2b74cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bc41464079c500adcbc76f85cff7f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a14eca9b1363403b1acd",
+    "url": "/static/css/2.b6ac9e22.chunk.css"
+  },
+  {
+    "revision": "3b9adc52f6bd4dec2da7",
+    "url": "/static/css/main.ea1027fc.chunk.css"
+  },
+  {
+    "revision": "a14eca9b1363403b1acd",
+    "url": "/static/js/2.3e9664a7.chunk.js"
+  },
+  {
+    "revision": "3b9adc52f6bd4dec2da7",
+    "url": "/static/js/main.6a340155.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
