0303b0d785398b5e6c39369101b4428aa4d98c0a
GO Lee <golee.dev@gmail.com>
2020-08-01 20:45:06 +0900

    fix footer and mainimage

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Row } from 'reactstrap';
+import './Footer.css';
 
 function Footer() {
+    const year = new Date().getFullYear();
+
     return (
-        <div style={{
-            height: '80px', display: 'flex',
-            flexDirection: 'column', alignItems: 'center',
-            justifyContent: 'center', fontSize: '.7rem',
-            fontWeight: '100'
-        }}>
-           <p> © Copyright 2020 by GO Lee </p>
+        <div className="footer__container">
+            <Row>
+                <a href=""><p>회사소개 |</p></a>
+                <a href=""><p>의뢰하기 |</p></a>
+                <a href=""><p>이용약관 |</p></a>
+                <a href=""><p>개인정보처리방침</p></a>
+            </Row>
+            <Row>
+                <h3>오성부동산</h3>
+                <p>주소 : 대표자 : 사업자 번호 : 부동산 번호 :</p>
+                <p>주소 : 대표자 : 사업자 번호 : 부동산 번호 :</p>
+                <p>오성부동산 © All rights reserved {year}</p>
+            </Row>
         </div>
     )
 }
--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -6,10 +6,13 @@ const MenuItemGroup = Menu.ItemGroup;
 function LeftMenu(props) {
   return (
     <Menu mode={props.mode}>
-    <Menu.Item key="mail">
-      <a href="/">Home</a>
+    <Menu.Item key="youtube">
+      <a href="https://www.youtube.com/channel/UCU76Tl3OEO9l0yGeYIJO5lg">오성TV</a>
+      </Menu.Item>
+    <Menu.Item key="blog">
+      <a href="https://m.blog.naver.com/PostList.nhn?blogId=olhun12">블로그</a>
     </Menu.Item>
-    <SubMenu title={<span>Blogs</span>}>
+    <SubMenu title={<span>매물</span>}>
       <MenuItemGroup title="Item 1">
         <Menu.Item key="setting:1">Option 1</Menu.Item>
         <Menu.Item key="setting:2">Option 2</Menu.Item>
